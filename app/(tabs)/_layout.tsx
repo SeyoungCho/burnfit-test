ddb72937e3b8ff7152905d9cfbbaa2974e5b258c
SeyoungCho <wnl383@naver.com>
2025-07-17 18:06:10 +0900

    feat: 바텀네비게이션 바 구현

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,70 @@
+import { Ionicons } from "@expo/vector-icons";
+import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
+import { Tabs } from "expo-router";
+
+export default function TabsLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: "#329EFF",
+        tabBarInactiveTintColor: "#8E8E93",
+        tabBarItemStyle: {
+          paddingTop: 8,
+        },
+        tabBarIconStyle: {
+          marginBottom: 6,
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "HOME",
+          tabBarIcon: ({ color, size, focused }) => (
+            <Ionicons
+              name={focused ? "home" : "home-outline"}
+              color={color}
+              size={size}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="calendar"
+        options={{
+          title: "CALENDAR",
+          tabBarIcon: ({ color, size, focused }) => (
+            <Ionicons
+              name={focused ? "calendar" : "calendar-outline"}
+              color={color}
+              size={size}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="library"
+        options={{
+          title: "LIBRARY",
+          tabBarIcon: ({ color, size, focused }) => (
+            <FontAwesome6 name="dumbbell" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="mypage"
+        options={{
+          title: "MY PAGE",
+          tabBarIcon: ({ color, size, focused }) => (
+            <Ionicons
+              name={focused ? "person" : "person-outline"}
+              color={color}
+              size={size}
+            />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
